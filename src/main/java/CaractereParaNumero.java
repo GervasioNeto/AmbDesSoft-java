1e845f8f74e24352d45b5b3bbe5e3cc4e2ff6f00
Gervásio Neto <gervasiolopesn@gmail.com>
2024-08-20 20:47:25 +0000

    CaractereParaNumero ALG1 e ALG2

--- src/main/java/CaractereParaNumero.java
+++ src/main/java/CaractereParaNumero.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+// ALG1
+
 public class CaractereParaNumero{
   Scanner sc = new Scanner(System.in);
 
@@ -7,7 +10,6 @@ public class CaractereParaNumero{
       String string = sc.next();
       int n = string.length();
       int decimal = 0;
-      // int base = Character.codePointAt(string, 0);
       int digito;
 
 
@@ -19,3 +21,25 @@ public class CaractereParaNumero{
       }
     }
 }
+
+// ALG2 Disponível no PDF AVA
+
+// public class CaractereParaNumero{
+//   Scanner sc = new Scanner(System.in);
+
+//     public void caractereNumero(){
+//       System.out.println("String numérica: ");
+//       String string = sc.next();
+//       int n = string.length();
+//       int decimal = 0;
+//       int base = Character.codePointAt(string, 0);
+//       int digito;
+
+
+//       for (int i = 0; i < n; i++){
+//         digito = Character.codePointAt(string, i) - base;
+//         decimal = decimal * 10 + digito;
+//       }
+//       System.out.printf("Formato decimal: %d%n", decimal);
+//     }
+// }
